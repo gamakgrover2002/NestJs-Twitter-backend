aac75df065fd04eb35fea6497ffdea2f5f3278b0
gamak grover <gamakgroverofficial@gmail.com>
2025-02-17 11:35:14 +0530

    sql db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './user.controller';
-
+import { UsersController } from './users/users.controller';
+import { PostsController } from './posts/posts.controller';
+import { HashtagsController } from './hashtags/hashtags.controller';
+import { TypeOrmModule } from '@nestjs/typeOrm';
+import dataSource from './db/sql.db';
 @Module({
-
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forRoot(dataSource)],
+  controllers: [UsersController, PostsController, HashtagsController],
 
 })
 export class AppModule {}
---
+++ src/db/sql.db.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeOrm'
+
+const dataSource: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '@8P20pm0133',
+  database: 'stock',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
+
+export default dataSource;
